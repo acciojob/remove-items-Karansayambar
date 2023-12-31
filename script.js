9851d0d745155b8e7dd5309eae3d17bd5fa43f5e
Karan Sayambar <86702398+Karansayambar@users.noreply.github.com>
2023-12-31 09:58:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+
+function selectAndRemoveColor(){
+	const selectColor = document.getElementById('colorSelect');
+	const selectedColor = selectColor.value;
+	selectColor.remove(selectColor.selectedIndex);
+}
